58f2431518c438cfe83b071f7d29d81d6aa8f66f
Thomas Karl <thomas@maksimer.no>
2018-11-29 19:47:24 +0100

    Winner page and deleting of unnecessary scss

--- assets/js/winner.js
+++ assets/js/winner.js
@@ -8,15 +8,16 @@ const winner = localStorage.getItem(winnerKey);
 
 
 /**
- * Creating the shields 🛡️
+ * Creating the shield 🛡️
  */
 
 const shieldWinner = document.querySelector('.shield-winner');
+const nameWinner = document.querySelector('.name-winner');
 
 let winnerName = document.createElement('p');
 winnerName.innerHTML = winner;
 winnerName.classList.add('winner-name');
-shieldWinner.appendChild(winnerName);
+nameWinner.appendChild(winnerName);
 
 let shield = document.createElement('img');
 shield.classList.add('shield');
@@ -37,3 +38,21 @@ if (winner === 'Quellon Greyjoy') { shield.src = shieldUrl + '8.svg'; winnershie
 if (winner === 'Gregor Clegane') { shield.src = shieldUrl + '9.svg'; winnershield = shield; }
 if (winner === 'Hoster Tully') { shield.src = shieldUrl + '10.svg'; winnershield = shield; }
 
+
+
+/**
+ * Starting the fireworks 🎆
+ */
+
+document.addEventListener("DOMContentLoaded", function (event) {
+// <!--Firework Credis to Scott Schiller @ http://www.schillmania.com/-->
+	var r = 4 + parseInt(Math.random() * 16);
+	for (var i = r; i--;) {
+		setTimeout('createFirework(8,14,2,null,null,null,null,null,Math.random()>0.5,true)', (i + 1) * (1 + parseInt(Math.random() * 1000)));
+		setTimeout('createFirework(25,187,5,1,null,null,null,null,false,true)', (i + 1) * (1 + parseInt(Math.random() * 1000)));
+	}
+	setTimeout(function(){
+		createFirework(100,800,8,5,1,100,50,50,false,false);return false
+	}, 8000);
+	return false
+});
